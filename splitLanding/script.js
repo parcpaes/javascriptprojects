5a8c500e664f9e52a32ce6133b472e2d8b81f93c
paes <spaesmx@gmail.com>
2021-04-22 21:16:23 -0400

    Spling landspace   transition left and right wihout using javascript

---
+++ splitLanding/script.js
@@ -0,0 +1,20 @@
+// const boxLeft = document.querySelector('.box-left');
+// const boxRight = document.querySelector('.box-right');
+
+// boxLeft.addEventListener('mouseenter', (e) => {
+//   boxLeft.classList.toggle('flex-75');
+//   boxRight.classList.toggle('flex-25');
+// });
+// boxLeft.addEventListener('mouseleave', (e) => {
+//   boxLeft.classList.toggle('flex-75');
+//   boxRight.classList.toggle('flex-25');
+// });
+
+// boxRight.addEventListener('mouseenter', (e) => {
+//   boxRight.classList.toggle('flex-75');
+//   boxLeft.classList.toggle('flex-25');
+// });
+// boxRight.addEventListener('mouseleave', (e) => {
+//   boxRight.classList.toggle('flex-75');
+//   boxLeft.classList.toggle('flex-25');
+// });
